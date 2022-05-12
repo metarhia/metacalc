56abafa289c5d79df1548cc56df712b615cee09a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-05-12 23:52:19 +0300

    Module structure
    PR-URL: https://github.com/metarhia/metacalc/pull/1

---
+++ test/unit.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const metatests = require('metatests');
+const { Sheet } = require('..');
+
+metatests.test('Sheet tests stub', async (test) => {
+  const sheet = new Sheet();
+  sheet.cells['A1'] = 100;
+  sheet.cells['B1'] = 2;
+  sheet.cells['C1'] = '=A1*B1';
+  sheet.values['C1'] = sheet.cells['A1'] * sheet.cells['B1'];
+  test.strictSame(sheet.values['C1'], 200);
+  test.end();
+});
