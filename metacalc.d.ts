52de9f88ff6cada3e91532fdb6b20840a7f33729
Кондрев Георгий <g.kondrev@eastwind.ru>
2022-07-29 18:33:30 +0500

    update Sheet interface

--- metacalc.d.ts
+++ metacalc.d.ts
@@ -1,6 +1,7 @@
+import { EventEmitter } from 'events';
 import { Context } from 'vm';
 
-export interface Sheet {
+export interface Sheet extends EventEmitter {
   data: Map<string, string | number>;
   expressions: Map<string, Function>;
   values: object;
